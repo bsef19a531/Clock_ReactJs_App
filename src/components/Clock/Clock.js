09ccbe73f630fbb4683175eacbd49845c7e1f048
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-07-24 15:18:39 +0500

    Finalized Clock Components

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import styles from "./Clock.module.css";
+import ClockDot from "./ClockDot/ClockDot";
 
 const Clock = () => {
 
     let time = new Date().toLocaleTimeString();
+    let day = new Date().getDay();
+    let date = new Date().getDate();
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth();
+
+    const monthList = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    const dayList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
     const [currentTime, setcurrentTime] = useState(time);
 
@@ -18,8 +26,11 @@ const Clock = () => {
     return (
         <div className={styles.clock}>
             <h1 className={styles.clock_heading}>React Clock</h1>
-            <div className={styles.clock_time}>
-                {currentTime}
+            <div className={styles.clock_border}>
+                <div className={styles.clock_time}>
+                    {currentTime}
+                </div>
+                <ClockDot dotValue="●" day={dayList[day]} month={monthList[month]} date={date++} year={year} />
             </div>
         </div>
     )
