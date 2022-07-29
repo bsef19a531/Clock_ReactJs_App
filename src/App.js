e568af62576d4e207c330cad8a4a8469f1f073bb
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-07-29 19:53:59 +0500

    Added Logic to switch Clocks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Clock from "./components/Clock/Clock";
 import Switch from "./components/Switch/Switch";
+import { useState } from "react";
 import AnalogueClock from "./components/AnalogueClock/AnalogueClock";
 
+let switchClock = true;
+
 function App() {
+
+  const [currentClock, setCurrentClock] = useState(switchClock);
+  const setClockType = (switchCondition) => {
+    setCurrentClock(switchCondition);
+    console.log("hit");
+  }
+
   return (
     <>
-      < Clock />
-      <Switch title="Light Mode" />
+      {currentClock ? < Clock /> : <AnalogueClock />}
+      <Switch title="Clock Mode" set={setClockType} />
       {/* <AnalogueClock /> */}
     </>
   );
--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,10 +1,17 @@
 import styles from "./Switch.module.css";
 
+let switchCondition = true;
 const Switch = (props) => {
+
+    const changeState = () => {
+        switchCondition = !switchCondition;
+        props.set(switchCondition);
+    }
+
     const styleSwitch = `form-check form-switch ${styles.switch}`;
     return (
         <div className={styleSwitch} >
-            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={changeState} />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault"> {props.title}</label>
         </div >
     )
